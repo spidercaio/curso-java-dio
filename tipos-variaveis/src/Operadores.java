5f8beb4bb8eef9aa2d7365cd1aa02f8625431fb1
spidercaio <cmsouzared@gmail.com>
2024-04-25 13:47:36 -0300

    adicionados exemplos de operadores de comparação

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,26 @@
+public class Operadores {
+
+    public static void main(String[] args) {
+
+        int numero1 = 4;
+        int numero2 = 7;
+
+        boolean simNao = numero1 == numero2;
+
+        System.out.println("Número um é igual a número dois?" + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("Número um é diferente de número dois?" + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("Número um é maior que número dois?" + simNao);
+
+        simNao = numero1 < numero2;
+
+        System.out.println("Número um é menor que número dois?" + simNao);
+
+    }
+
+}

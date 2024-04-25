af326c5d11203efbdfed6a6c66cc4faed10f7f3d
spidercaio <cmsouzared@gmail.com>
2024-04-25 13:36:12 -0300

    treino de operadores

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
+        public class TiposVariaveis {
+            /**
+             * @param args
+             * @throws Exception
+             */
+            public static void main(String[] args) throws Exception {
+                int a = 7;
+                int b = 9;
+                /* a configuração ? : pode ser utilizada para substituir o if e o else
+                String resultado = " ";
+                if (a==b)
+                resultado = "verdadeiro";
+                else
+                resultado = "falso";
+                System.out.println(resultado); */
+
+                String resultado = a==b ? "verdadeiro" : "falso";
+                System.out.println(resultado);
+
+            
+            }
+        }
